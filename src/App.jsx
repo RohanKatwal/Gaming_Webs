cfd9956836a4fc9472b282f16dffcd6543bc59b7
RohanKatwal <rohanwork909@gmail.com>
2025-04-22 14:49:20 +0545

    New method font and ttheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1 className="font-zentry text-3xl">Zentry Font</h1>
+      <p className="font-general">General Font</p>
+      <p className="font-circular-web">Circular Web Font</p>
+      <p className="font-robert-medium">Robert Medium Font</p>
+      <p className="font-robert-regular">Robert Regular Font</p>
 
-export default App
+      {/* Colors */}
+      <div className="bg-blue-50 text-blue-300">
+        Blue 50 Background, Blue 300 Text
+      </div>
+      <div className="bg-violet-300 text-white">Violet 300 Background</div>
+      <div className="bg-yellow-300 text-yellow-100">
+        Yellow 300 Background, Yellow 100 Text
+      </div>
+    </>
+  );
+};
+
+export default App;
