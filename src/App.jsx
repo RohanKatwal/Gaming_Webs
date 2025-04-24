74c7a8dbf9af3d7084656a8e96385d8d198a744c
RohanKatwal <rohanwork909@gmail.com>
2025-04-24 14:03:14 +0545

    test bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import './index.css'
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <>
-      <h1 className="font-zentry text-3xl">Zentry Font</h1>
-      <p className="font-general">General Font</p>
-      <p className="font-circular-web">Circular Web Font</p>
-      <p className="font-robert-medium">Robert Medium Font</p>
-      <p className="font-robert-regular">Robert Regular Font</p>
-
-      {/* Colors */}
-      <div className="bg-blue-50 text-blue-300">
-        Blue 50 Background, Blue 300 Text
-      </div>
-      <div className="bg-violet-300 text-white">Violet 300 Background</div>
-      <div className="bg-yellow-300 text-yellow-100">
-        Yellow 300 Background, Yellow 100 Text
-      </div>
-    </>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
   );
 };
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='video-frame' className='relative z-10 h-dvh w-screen'>
+            <div className='mask-clip-path'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
